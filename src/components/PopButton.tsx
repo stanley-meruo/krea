54334acc10b3d0ddff1f7871c4c3877f053b2daf
Stanley Meruo <144041200+stanley-meruo@users.noreply.github.com>
2025-09-24 12:26:55 +0100

    Feat: Remove padding from GallerySection

--- src/components/PopButton.tsx
+++ src/components/PopButton.tsx
@@ -5,11 +5,11 @@ const PopButton = () => {
 
     return (
       <div className="fixed bottom-5 right-4 flex gap-2 xs:right-5 sm:right-8 lg:right-12 xl:right-14 xxl:right-16">
-        <button className="flex items-center gap-1 bg-gray-100 p-1.5 rounded-xl shadow transition text-sm lg:text-base">
+        <button className="flex items-center gap-1 bg-gray-200 p-1.5 rounded-xl shadow transition text-sm lg:text-base">
           <IoDocumentText />
           Legal
         </button>
-        <button className="flex items-center gap-1 bg-gray-100 p-1.5 rounded-xl shadow transition text-sm lg:text-base">
+        <button className="flex items-center gap-1 bg-gray-200 p-1.5 rounded-xl shadow transition text-sm lg:text-base">
           <BsCreditCardFill />
           Pricing
         </button>
